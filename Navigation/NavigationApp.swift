0ce2378ba8cd8441fa646a797709b89e6f2f0770
Olivia Marcolini <oliviamonteiro2406@gmail.com>
2025-07-10 11:00:52 -0300

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Olivia Monteiro on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
